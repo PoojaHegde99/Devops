cae6e9e62fdd06272a9e87b6a3a6cae554e5df8a
Devops <91864330+PoojaHegde99@users.noreply.github.com>
2021-12-24 19:31:14 +0530

    Update ChatbotTest.java

--- ChatApplication-main/src/test/java/com/assignment/oopd/ChatbotTest.java
+++ ChatApplication-main/src/test/java/com/assignment/oopd/ChatbotTest.java
@@ -17,24 +17,9 @@ public class ChatbotTest {
 
     @Test
     public void init(){
-        logger.info("Running a dummyTest");
+       console.log("Hello world!!")
     }
-        @Test
-    void whenTestCase1_thenPrintTest1_1() {
-        logger.info("Running When Case1: test1_1");
-    }
-
-    @Test
-    void whenTestCase1_thenPrintTest1_2() {
-        logger.info("Running When Case1: test1_2");
-    }
-
-    @Test
-    void whenTestCase1_thenPrintTest1_3() {
-        logger.info("Running When Case1: test1_3");
-    }
-        
-    
+       
 
     public void initBrowser(){
         System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
